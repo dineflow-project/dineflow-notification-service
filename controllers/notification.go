81bb24dea32d73840088c4c042594da797f3633b
Chanawat Voravijitchaikul <nanae11879@gmail.com>
2023-10-20 18:11:28 +0700

    implement notification service

---
+++ controllers/notification.go
@@ -0,0 +1,80 @@
+package controllers
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"os"
+
+	"github.com/dineflow-project/dineflow-notification-service/database"
+	"github.com/dineflow-project/dineflow-notification-service/models"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+func GetNotificationsHandler(c *gin.Context) {
+	recipientID := c.Param("recipientID")
+	collection := database.Client.Database("noti-service").Collection("notifications")
+
+	cursor, err := collection.Find(context.TODO(), bson.M{"recipient_id": recipientID})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch notifications"})
+		return
+	}
+	defer cursor.Close(context.TODO())
+
+	var notifications []models.Notification
+	for cursor.Next(context.TODO()) {
+		var notification models.Notification
+		if err := cursor.Decode(&notification); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to decode notification"})
+			return
+		}
+		notifications = append(notifications, notification)
+	}
+
+	// Update is_read of the notifications to true
+	_, err = collection.UpdateMany(
+		context.TODO(),
+		bson.M{"recipient_id": recipientID, "is_read": false},
+		bson.M{"$set": bson.M{"is_read": true}},
+	)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update notifications"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"notifications": notifications})
+}
+
+func GetUnreadNotificationsCountHandler(c *gin.Context) {
+	recipientID := c.Param("recipientID")
+	collection := database.Client.Database("noti-service").Collection("notifications")
+
+	count, err := collection.CountDocuments(context.TODO(), bson.M{
+		"recipient_id": recipientID,
+		"is_read":      false,
+	})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch notifications count"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"count": count})
+}
+
+func UpdateNotifications(msg string) error {
+	var notification models.Notification
+	err := json.Unmarshal([]byte(msg), &notification)
+	if err != nil {
+		return err
+	}
+
+	collection := database.Client.Database(os.Getenv("MONGO_DATABASE")).Collection("notifications")
+	_, err = collection.InsertOne(context.Background(), notification)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ rabbitmq/consumer.go
@@ -0,0 +1,56 @@
+package rabbitmq
+
+import (
+	"log"
+
+	"github.com/dineflow-project/dineflow-notification-service/controllers"
+	"github.com/streadway/amqp"
+)
+
+func StartNotificationConsumer(amqpURL string, queueName string) {
+	// Connect to RabbitMQ server
+	conn, err := amqp.Dial(amqpURL)
+	if err != nil {
+		log.Fatalf("Failed to connect to RabbitMQ: %v", err)
+	}
+	defer conn.Close()
+
+	// Create a channel
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("Failed to open a channel: %v", err)
+	}
+	defer ch.Close()
+
+	// Declare a queue
+	q, err := ch.QueueDeclare(
+		queueName, // name
+		false,     // durable
+		false,     // delete when unused
+		false,     // exclusive
+		false,     // no-wait
+		nil,       // arguments
+	)
+	if err != nil {
+		log.Fatalf("Failed to declare a queue: %v", err)
+	}
+
+	// Consume messages from the queue
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	if err != nil {
+		log.Fatalf("Failed to register a consumer: %v", err)
+	}
+
+	for msg := range msgs {
+		log.Printf("Received a message: %s", msg.Body)
+		controllers.UpdateNotifications(string(msg.Body))
+	}
+}
